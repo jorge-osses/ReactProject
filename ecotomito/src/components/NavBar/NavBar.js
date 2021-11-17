73f37a17ac1662eb6560b582b5ec26a803929686
Jorge Osses <jorgitoosses@gmail.com>
2021-11-17 00:45:38 -0300

    Create NavBar js

---
+++ ecotomito/src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="listNavBar">
+                <li><a href="/#">Menu</a></li>
+                <li><a href="/#">Contacto</a></li>
+                <li><a href="/#">Productos</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default NavBar;
